7d818d2cffdd774b2e904f744e1a8b83a6066d00
Leodeng <71826908+leodenglovescode@users.noreply.github.com>
2026-05-26 04:16:32 +0800

    fix: Fix config

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
+export default defineConfig(({ isSsrBuild }) => ({
   plugins: [vue(), tailwindcss()],
   build: {
     rollupOptions: {
-      output: {
+      output: isSsrBuild ? {} : {
         manualChunks: {
           'highlight': ['highlight.js/lib/common'],
           'marked': ['marked'],
@@ -14,4 +14,4 @@ export default defineConfig({
       }
     }
   }
-})
+}))
